72c85116fa58a33774037ae6b1860bd19916446d
yahyaaq6 <95889839+yahyaaq6@users.noreply.github.com>
2022-01-27 18:25:08 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var button2;
+var angle=60;
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  button = createImg('up.png');
+  button.position(20,30);
+  button.size(50,50);
+  button.mouseClicked(aforce);
+  
+
+  ground = Bodies.rectangle(100,400,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  ground1 = Bodies.rectangle(100,250,100,20,ground_options);
+  World.add(world,ground1);
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  Matter.Body.rotate(ground1,angle);
+ push ()
+ translate (ground1.position.x,ground1.position.y);
+ rotate (angle);
+
+  rect(0,0,100,20);
+  pop ();
+  angle+=0.1;
+  
+ 
+
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,500,20);
+ 
+console.log(ground.position.y);
+
+  
+  
+}
+
+function aforce()
+{
+Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+
+}
